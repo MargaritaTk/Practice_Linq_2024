98788cbf2f91e6e1c38e17a8658cc759db480ba9
Margarita Tkachenko <margosha2413@gmail.com>
2024-12-13 18:30:04 +0200

    Реалізація запиту №1

--- Practice_Linq_2024/Program.cs
+++ Practice_Linq_2024/Program.cs
@@ -46,13 +46,19 @@ namespace Practice_Linq_2024
         {
             //Query 1: Вивести всі матчі, які відбулися в Україні у 2012 році.
 
-            var selectedGames = games; // Корегуємо запит !!!
+            var selectedGamesInUkraine = games
+            .Where(g => g.Country == "Ukraine" && g.Date.Year == 2012)
+            .ToList();
 
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 1 ========================");
 
             // див. приклад як має бути виведено:
+            foreach (var game in selectedGamesInUkraine)
+            {
+                Console.WriteLine($"{game.Date:dd.MM.yyyy} {game.Home_team} - {game.Away_team}, Score: {game.Home_score}-{game.Away_score}, Country: {game.Country}");
+            }
 
 
         }
